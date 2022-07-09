b46626d21a9eff8076e7648153b9f7b3562828c8
Skandarella <75903112+Skandarella@users.noreply.github.com>
2022-07-09 22:36:46 +0200

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,19 @@
+
+-- Load support for intllib.
+local path = minetest.get_modpath(minetest.get_current_modname()) .. "/"
+
+local S = minetest.get_translator and minetest.get_translator("naturalbiomes") or
+		dofile(path .. "intllib.lua")
+
+
+-- biomes
+
+dofile(path .. "wetsavanna.lua") --
+dofile(path .. "alderswamp.lua") --
+dofile(path .. "alpine.lua") --
+dofile(path .. "bambooforest.lua") --
+dofile(path .. "palmbeach.lua") --
+dofile(path .. "outback.lua") --
+dofile(path .. "mediterranean.lua") --
+dofile(path .. "crafting.lua") --
+dofile(path .. "hunger.lua") --
